bceb341e612f810fc1527fde538661d8239c646e
Alan Pereira <alpereira@questrade.com>
2023-09-22 16:00:35 -0300

    Revert "importer"
    This reverts commit e387f37471dbc5599048ed79e5543f3f358222e6.

--- src/config.js
+++ src/config.js
@@ -1,29 +1,7 @@
 const config = {
-    numberOfThreads: 2,
-
-    oneTrust: {
-        baseUrl: "https://app-ca.onetrust.com/api",
-        authUrl: "/access/v1/oauth/token",
-        getProfilesUrl: "/consentmanager/v1/datasubjects/profiles",
-        getLinkTokenUrl: "/consentmanager/v1/linktokens",
-        auth: {
-            grantType: "client_credentials",
-            clientId: "",
-            clientSecret: ""
-        }
-    },
-
-    marketo: {
-        baseUrl: "https://463-ATS-282.mktorest.com",
-        authUrl: "/identity/oauth/token",
-        getLeadUrl: "/rest/v1/leads.json",
-        updateLeadUrl: "/rest/v1/leads.json",
-        auth: {
-            grantType: "client_credentials",
-            clientId: "",
-            clientSecret: ""
-        }        
-    }
+    numberOfThreads: 8
 };
+
+
 
 module.exports = { config }
--- src/worker.js
+++ src/worker.js
@@ -1,71 +1,21 @@
-const { getLinkToken } = require('./onetrust-client');
-const { getLeads, updateLeads } = require('./marketo-client');
-const { buildMarketoConsent } = require('./builder');
+const { parentPort, workerData } = require('worker_threads');
+const { createReceipt } = require('./onetrust');
 
 async function execute(records, child) {
     const results = [];
-    const reprocess = [];
-    let hasReprocessed = false;
-
     let processedCount = 0;
 
     for (const record of records) {
-        console.log(`[${child}][Record=${processedCount+1}] - Starting`);
-        const startTime = new Date();   
-
-        const emailAddress = record.DataElements.find(de => de.Name === "Email Address")?.Value;        
-        if (emailAddress === undefined)  {
-            continue;        
-        }
-            
-
-        const hasCasl = record.Purposes.find(p => p.Name === "CASL Consent");
-        if (!hasCasl) {
-            continue;
-        };
-
-        const identifier = record.Identifier;
-        const token = await getLinkToken(identifier);
-        
-        const leads = await getLeads(emailAddress);
-        if (leads.success === false && leads.errors?.find(err => err.code === "1003")) continue;        
-        if ((leads.success === true && leads.result.lengt) === 0 || !leads.result) {
-            console.log("Pushing to reprocess");
-            reprocess.push(record);
-            continue;
-        };
-        
-        const consent = buildMarketoConsent(emailAddress, leads, record.Purposes, token);
-      
-        const updateResult = await updateLeads(consent);
-        const updated = updateResult.result?.find(r => r.status === "updated");
-
-        if (!updated) {
-            console.log("Pushing to reprocess");
-            reprocess.push(record);
-            continue;
-        }
+        const startTime = new Date();
 
         const endTime = new Date();
         const elapsedTime = endTime - startTime;
-        console.log(`[${child}][Record=${processedCount}] - Elapsed Time: ${elapsedTime}`);
-        results.push(updated);
+        console.log(`[${child}] - Elapsed Time: ${elapsedTime}`);
+        results.push(result);
 
         processedCount++;
     }
-
-    let reprocessResults = [];
-    if (reprocess.length > 0 && !hasReprocessed) {
-        hasReprocessed = true; 
-        setTimeout(async () => {
-          console.log(`[${child}] - Initiating reprocessing after 20 minutes...`);
-          reprocessResults = await execute(reprocess, child);         
-  
-          console.log(`[${child}] - Reprocessing completed.`);
-        }, 20 * 60 * 1000); 
-      }
-
-    return results.concat(reprocessResults);
+    return results;
 }
 
 process.on("message", async (message) => {
